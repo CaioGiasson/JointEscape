7382c2fd65a8f1fd8e2abf29316e396bb1e6eb47
CaioGiasson <cainitech@gmail.com>
2020-06-23 23:49:19 -0300

    Primeiro commit

---
+++ Assets/Scripts/Corrente.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corrente : MonoBehaviour {
+
+	public GameObject bola;
+	public bool inverted;
+
+	private Rigidbody2D bolaRB;
+	private float intervalo, intensidade;
+
+	private void Start ( ) {
+
+		bolaRB = bola.GetComponent<Rigidbody2D> ( );
+		intervalo = Random.Range ( 3, 6 );
+		intensidade = Random.Range ( -600, -300 );
+
+		StartCoroutine ( Chicotear ( ) );
+	}
+
+	IEnumerator Chicotear ( ) {
+		bolaRB.AddForce ( new Vector2 ( intensidade, 0 ) );
+		yield return new WaitForSeconds ( intervalo );
+		StartCoroutine ( Chicotear ( ) );
+	}
+
+	private void FixedUpdate ( ) {
+		if ( inverted ) bolaRB.AddForce ( new Vector2 ( 0, 1000 ) );
+	}
+}
---
+++ Assets/Scripts/Mago.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mago : MonoBehaviour {
+
+	public float velox;
+	public GameObject gameOverUI;
+
+	private bool acabou = false;
+
+	private void Start ( ) {
+		gameOverUI.SetActive ( false );
+	}
+
+	void Update ( ) {
+
+		if ( acabou ) return;
+
+		if ( Input.GetKey ( KeyCode.W ) && transform.position.y < 2.5 ) transform.position += new Vector3 ( 0, velox );
+		if ( Input.GetKey ( KeyCode.A ) && transform.position.x > -7.5 ) transform.position += new Vector3 ( -velox, 0 );
+		if ( Input.GetKey ( KeyCode.S ) && transform.position.y > -2.5 ) transform.position += new Vector3 ( 0, -velox );
+		if ( Input.GetKey ( KeyCode.D ) && transform.position.x < 7.5 ) transform.position += new Vector3 ( velox, 0 );
+
+	}
+
+	private void OnCollisionEnter2D ( Collision2D collision ) {
+		if ( collision.gameObject.CompareTag ( "corrente" ) ) Reiniciar ( );
+	}
+
+	private void OnTriggerEnter2D ( Collider2D collision ) {
+		if ( collision.gameObject.CompareTag ( "Finish" ) ) GameOver ( );
+	}
+
+	public void Reiniciar ( ) {
+		acabou = false;
+		SceneManager.LoadScene ( SceneManager.GetActiveScene ( ).buildIndex );
+	}
+
+	void GameOver ( ) {
+		acabou = true;
+		gameOverUI.SetActive ( true );
+	}
+}
